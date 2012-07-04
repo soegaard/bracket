4c8e1866e5550bb363c777af1ba06b2b347b530e
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-04 23:54:23 +0200

    Added checks of solutions.

--- bracket/examples/equation-solving.rkt
+++ bracket/examples/equation-solving.rkt
@@ -1,8 +1,17 @@
 #lang bracket
 DeclareVars(x,a,b,c);
 
-Solve(2*x=1,x);   % x = 1/2
+Solve(2*x=1,x);                   % x = 1/2
+Substitute(2*x=1,Solve(2*x=1,x)); % 1=1 
 
 Solve(2*x+3=1,x); % x = -1
 
 Solve(a*x+b=c,x); % x = (c-b)/a
+
+solution:=Solve(a*x+b+x=c,x);
+solution;
+% Check solution
+Expand(Substitute(a*x+b+x,solution)); % This actually simplifies to c
+% ... unfortunately not automatically.
+
+
